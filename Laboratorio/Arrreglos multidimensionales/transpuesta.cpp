5804b56851658a3112538fa2234ae99831a44789
JNaranjo12 <juan.naranjo01@epn.edu.ec>
2023-02-07 12:43:15 -0500

    TRAZA

--- Laboratorio/Arrreglos multidimensionales/transpuesta.cpp
+++ Laboratorio/Arrreglos multidimensionales/transpuesta.cpp
@@ -5,14 +5,29 @@ using namespace std;
 const int M= 3, N=3;
 void imprimir(const int A[M][N]);
 void transpuesta(const int A[M][N], int B[M][N]);
+void suma(int A[M][N], int B[M][N], int C[M][N]);
+void multiplicacion(int A[M][N], int B[M][N], int D[M][N]);
+int traza(int D[M][N]);
 int main(){
     int A[M][N]=    {{1,2,3},
                     {4,5,6},
                     {7,8,9}};
     int B[M][N];
+    int C[M][N];
+    int D[M][N];
     imprimir(A);
+    cout << endl;
     transpuesta(A,B);
     imprimir(B);
+    cout << endl;
+    suma(A,B,C);
+    imprimir(C);
+    cout << endl;
+    multiplicacion(A,B,D);
+    imprimir (D);
+    cout << endl;
+
+    cout << "La traza es: " << traza(D) << endl;
     return 0;
 }
 
@@ -32,3 +47,36 @@ void transpuesta(const int A[M][N], int B[M][N]){
         }
     }
 }    
+
+void suma( int A[M][N], int B[M][N], int C[M][N]){
+    for (int i = 0; i < M; i++){
+        for(int j = 0; j < N; j++){
+            C[i][j] = A[i][j] + B[i][j];
+            //cout << C[i][j] << endl;
+        }
+    }
+}
+
+void multiplicacion(int A[M][N], int B[M][N], int D[M][N]){
+    int Q = 3;
+    for (int i = 0; i < M; i++){
+        for(int j = 0; j < N; j++){
+            for (int k = 0; k < Q; k++){
+                D[i][j] = 0;
+                 D[i][j] = D[i][j] +  (A[i][k] * B[i][j]);
+            }
+        }
+    }
+}
+
+int traza(int D[M][N]){
+    int resultado = 0;
+    for (int i = 0; i < M; i++){
+        for(int j = 0; j < N; j++){
+            if(i == j){
+                resultado += D[i][j];
+            }
+        }
+    }
+    return resultado;
+}
